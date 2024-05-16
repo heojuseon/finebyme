8532510c3566ea05c6898c32eea4df32f0c89921
heojuseon <hjs2683@naver.com>
2024-05-16 15:07:25 +0900

    splashscreen and main_ui 작업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
+    // Splash
+    implementation ("androidx.core:core-splashscreen:1.0.1")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/finebyme/MainActivity.kt
+++ app/src/main/java/com/example/finebyme/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.finebyme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.finebyme.databinding.ActivityMainBinding
+import com.example.finebyme.view.FavoriteImgFragment
+import com.example.finebyme.view.ImageListFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        // SplashScreen을 적용한다.
+        // setContentView 전에 작성해야 한다.
+        installSplashScreen()
+
+
+        setBottomNavigationView()
+
+        //앱 초기 실행 시 메인화면 설정
+        if (savedInstanceState == null){
+            binding.bottomNavView.selectedItemId = R.id.fragment_image_list
+        }
+
+        setContentView(view)
+    }
+
+    private fun setBottomNavigationView() {
+        binding.bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_image_list -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, ImageListFragment()).commit()
+                    true
+                }
+                R.id.fragment_favorite_img -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, FavoriteImgFragment()).commit()
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
     }
 }
